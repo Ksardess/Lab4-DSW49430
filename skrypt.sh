c7ea14a3a7da67c84a1f73a1291377d0f83286fc
Ksardess <adam.andrasz123@gmail.com>
2023-05-09 16:02:55 +0200

    update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,14 @@
 
 if [[ $1 == "--date" ]]; then
   date
+elif [[ $1 == "--logs" ]]; then
+  for ((i=1; i<=100; i++))
+  do
+    filename="log${i}.txt"
+    echo "Nazwa pliku: $filename" > "$filename"
+    echo "Nazwa skryptu: $0" >> "$filename"
+    echo "Data utworzenia: $(date)" >> "$filename"
+  done
 else
-  echo "Nieprawidłowa opcja. Użyj --date, aby wyświetlić aktualną datę."
+  echo "Nieprawidłowa opcja. Użyj --date, aby wyświetlić aktualną datę lub --logs, aby utworzyć 100 plików logx.txt."
 fi
